14f7d1216068ea47622793556d547dcd1dd05c70
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-11-13 10:51:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import os
 from tensorflow import keras
 import tensorflow as tf
 
-from classfication import pred
+
 app=Flask(__name__)
 
- 
+model=keras.models.load_model("model.h5")
+classes_name={0:"Choroidal neovascularization",1:"Diabetic macular edema ",2:"Drusen",3:"Normal"} 
 
 app.secret_key = "secret key"
 
@@ -23,9 +24,16 @@ def predection():
         file=request.files["image"]
         file.save(file.filename)
         img=tf.keras.utils.load_img(file.filename)
-        state=pred(img)
-        os.remove(file.filename)
-        return str(state)
+        img=tf.keras.utils.img_to_array(img)
+        image=tf.image.resize(img,(160,160))
+        image = np.expand_dims(image, axis=0)
+        image=image/255.
+        predection=model.predict(image)
+        pre=predection.flatten()
+        m=pre.max()
+        pre=list(pre)
+        return str(classes_name[pre.index(m)])   
+          
 
         
 
